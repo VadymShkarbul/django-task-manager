69aabfb8297de59b6dc91aaa8b0b4c4e04d14c20
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-14 12:35:59 +0200

    Added crispy forms to project

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
     "task_manager",
 ]
 
@@ -68,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = "django_practice.wsgi.application"
 
